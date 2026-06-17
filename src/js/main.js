bf44744b8abff4841f2833f9ca299f44c66c87ec
NAIMEDINA380 <naysonmedina380@gmail.com>
2026-06-17 08:50:51 -0400

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,13 @@
 document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault();
-
     const user = document.getElementById('username').value;
     const pass = document.getElementById('password').value;
     const message = document.getElementById('message');
 
-    if (user === "NAYMEDINA12" && pass === "Nayson123*") {
-        message.style.color = "green";
-        message.textContent = "Acceso concedido. Redirigiendo...";
-        setTimeout(() => {
-            window.location.href = "dashboard.html";
-        }, 1500);
+    if(user === 'NAYMEDINA12' && pass === 'Nayson123*') {
+        window.location.href = 'dashboard.html';
     } else {
-        message.style.color = "red";
-        message.textContent = "Usuario o contraseña incorrectos.";
+        message.style.color = 'red';
+        message.innerText = 'Usuario o contraseña incorrectos';
     }
 });
